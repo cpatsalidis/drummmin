5ab5316ede13cd1d2ef9b078e1102553adfade1d
cpatsalidis <christos.patsalidis20@imperial.ac.uk>
2025-02-15 12:00:00 +0200

    added method to remove 'playing' class

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
+const keys = Array.from(document.querySelectorAll('.key'))
+
+function removeTransistion(e) {
+    if (e.propertyName !== 'transform') return
+    e.target.classList.remove('playing')
+}
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransistion))
 window.addEventListener('keydown', (e) => {
     const audio = document.querySelector(`audio[data-key=${e.code}]`)
     const key = document.querySelector(`div[data-key=${e.code}]`)
     if (!audio) return
-    console.log(audio)
     audio.play()
     audio.currentTime = 0
     key.classList.add('playing')
-
 })
+
