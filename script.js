768c58fc104d3d331bec5671a3aea7b1e6b28346
cpatsalidis <christos.patsalidis20@imperial.ac.uk>
2025-02-15 12:00:00 +0200

    added audio play and key style transition on keydown

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 window.addEventListener('keydown', (e) => {
-    console.log(e.code)
-    const key = document.querySelector(`audio[data-key="${e.code}"]`)
+    const audio = document.querySelector(`audio[data-key=${e.code}]`)
+    const key = document.querySelector(`div[data-key=${e.code}]`)
+    if (!audio) return
+    console.log(audio)
+    audio.play()
+    audio.currentTime = 0
+    key.classList.add('playing')
+
 })
